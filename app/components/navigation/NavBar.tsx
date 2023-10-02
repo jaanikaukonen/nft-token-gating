1b3a9362f5c74e641d34076eb35efd7a6749311b
Jaani Kaukonen <kaukonen.jaani@gmail.com>
2023-10-02 12:51:54 +0300

    Add styling for navigation bar.

--- app/components/navigation/NavBar.tsx
+++ app/components/navigation/NavBar.tsx
@@ -1,19 +1,30 @@
+"use client"
+
 import React from "react"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+import Image from "next/image"
 import { navLinks } from "../../utils/navigation/data"
+import blockchainIcon from "../../../public/blockchain.svg"
 import styles from "./navbar.module.css"
-import logo from "../../../public/blockchain.svg"
 
 export default function NavBar() {
+    const pathname = usePathname()
+
     return(
         <header className={styles.header}>
             <div>
+                <Image 
+                    priority
+                    src={blockchainIcon}
+                    alt="Blockchain Icon"
+                />
             </div>
             <nav className={styles.nav}>
                 <ul className={styles.navlinks}>
                     {navLinks.map((link, index) => (
-                        <li className={styles.link} key={index}>
-                            <Link href={link.path}>{link.name}</Link>
+                        <li className={pathname === link.path ? `${styles.link} ${styles.active}` : styles.link} key={index}>
+                            <Link className={pathname === link.path ? `${styles.linkText} ${styles.linkActive}` : styles.linkText} href={link.path}>{link.name}</Link>
                         </li>
                     ))}
                 </ul>
