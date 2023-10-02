c46322c621a8582a0eb211c095ded3bb38b133af
Jaani Kaukonen <kaukonen.jaani@gmail.com>
2023-10-02 14:41:28 +0300

    Add styling for home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+"use client"
+
+import Image from "next/image"
 import styles from "./page.module.css"
+import metamaskIcon from "../public/metamask.svg"
 
 export default function Home() {
+  
+    const connectWallet = () => {
+        console.log("Trying to connect wallet")
+    }
+
     return (
         <main className={styles.main}>
-            <h1>Home / NFT Minting</h1>
+            <h1 className={styles.title}>Mint Your NFT, Unlock Exclusive Token-Gated Access</h1>
+            <div className={styles.card}>
+                <Image
+                    priority
+                    src={metamaskIcon}
+                    alt="Metamask Icon" />
+
+                <button className={styles.connectButton} onClick={connectWallet}>Click to connect your MetaMask Wallet</button>
+            </div>
         </main>
     )
 }
